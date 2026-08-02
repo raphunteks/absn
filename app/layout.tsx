27bc3fc97abd5e5d18009337a6ef4e2598481f01
raphunteks <ksanihtest@gmail.com>
2026-08-02 12:30:14 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import './globals.css' // <-- BARIS INI SANGAT WAJIB ADA
 
 export const metadata: Metadata = {
   title: 'AXAXYZ Attendance',
